e88063c4399fe47291235c3846265b3dbd383172
Christopher Pattullo <christopher.pat2013@gmail.com>
2026-02-21 00:20:52 -0800

    Finished hardware and firmware setup for reading moisture data from plant

---
+++ firmware/src/Smart-Plant-Firmware/main/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+//internal LED
+#define LED_PIN GPIO_NUM_42
+#define MOISTURE_INPUT_GPIO 20
+
+void app_main(void)
+{
+    gpio_config_t led_config = {
+        .pin_bit_mask = (1ULL << LED_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&led_config);
+
+    adc_unit_t unit;
+    adc_channel_t channel;
+    ESP_ERROR_CHECK(adc_oneshot_io_to_channel(MOISTURE_INPUT_GPIO, &unit, &channel));
+
+    adc_oneshot_unit_handle_t adc_handle;
+    adc_oneshot_unit_init_cfg_t unit_cfg = {
+        .unit_id = unit,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&unit_cfg, &adc_handle));
+
+    adc_oneshot_chan_cfg_t chan_cfg = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_12,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc_handle, channel, &chan_cfg));
+
+    int state = 0;
+    int raw = 0;
+
+    while (1) {
+        gpio_set_level(LED_PIN, state);
+        state ^= 1;
+        vTaskDelay(raw / 10);
+        adc_oneshot_read(adc_handle, channel, &raw);
+        printf("Moisture raw = %d\n", raw);
+    }
+    
+}
